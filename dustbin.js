fee3f693af4203545149810738b44861652d10b6
rich123tyr <70693161+rich123tyr@users.noreply.github.com>
2020-09-24 19:24:15 +0530

    Add files via upload

--- dustbin.js
+++ dustbin.js
@@ -8,6 +8,7 @@ class Dustbin {
       this.body = Bodies.rectangle(x, y, width, height, options);
       this.width = width;
       this.height = height;
+      this.image = loadImage("dustbingreen.png");
       
       World.add(world, this.body);
     }
@@ -20,7 +21,8 @@ class Dustbin {
       rotate(angle);
       rectMode(CENTER);
       fill(255);
-      rect(0,0, this.width, this.height);               
+      rect(0,0, this.width, this.height);   
+      image(this.image, 0, 0, this.width, this.height);            
       pop();
     }
   }
--- paper.js
+++ paper.js
@@ -7,6 +7,7 @@ class Paper{
             density:1.2,
         }
         this.body = Bodies.circle(x, y, 10,options);
+        this.Image = loadImage("paper.png")
         World.add(world, this.body);
     }
     display(){
@@ -16,6 +17,7 @@ class Paper{
         translate(pos.x,pos.y)
         angleMode(RADIANS);
         rotate(angle);
+        image(this.image,0,0,this.widt)
         circle(0,0,);  
         fill(255);
 
